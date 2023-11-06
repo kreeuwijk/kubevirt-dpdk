aa63837a89bfb081c1001ef9b7611d3b0fba6ace
Kevin Reeuwijk <kevin@spectrocloud.com>
2023-11-06 16:39:46 +0100

    don't use function

--- pkg/virt-controller/services/rendervolumes.go
+++ pkg/virt-controller/services/rendervolumes.go
@@ -475,7 +475,7 @@ func withVhostuserVolume(VhostuserSocketDir string) VolumeRendererOption {
 			// },
 			VolumeSource: k8sv1.VolumeSource{
 				HostPath: &k8sv1.HostPathVolumeSource{
-					Path: filepath.Dir("/var/run/openvswitch"),
+					Path: "/tmp/vhost_sockets",
 				},
 			},
 		})
